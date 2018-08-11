c4202a628f40f4390bddac7d1632190e95d08238
dmitrymishchenko <dimdos95@gmail.com>
2018-08-11 09:57:40 +0300

    ~ [ App ]: ~ fix routing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import TopBar from './components/TopBar';
 import TagCloud from './components/TagCloud';
 
 const App = () => (
     <React.Fragment>
-        <TopBar />
-        <TagCloud />
+        <Route path="/" component={TopBar} />
+        <Route path="/" component={TagCloud} />
     </React.Fragment>
 );
 
